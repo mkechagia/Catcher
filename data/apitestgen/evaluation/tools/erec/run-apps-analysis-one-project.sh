3d447b39d20ba3f355a7a9980d32b70e293da55e
Lidia Ataupillco <ataupill@ualberta.ca>
2021-12-08 10:33:03 -0700

    fixed indentations in shell script

--- data/apitestgen/evaluation/tools/erec/run-apps-analysis-one-project.sh
+++ data/apitestgen/evaluation/tools/erec/run-apps-analysis-one-project.sh
@@ -29,7 +29,7 @@ do
 	then
 		echo "$FILE is a subdirectory..."
 
-        	cd $FILE
+		cd $FILE
 
 		#create .jar with dependencies
 		$cwd/evaluation/tools/erec/apps/deps.sh $FILE
@@ -39,17 +39,17 @@ do
 
 		python3 $cwd/evaluation/tools/erec/api/jdk_call_graph.py $cwd/evaluation/subjects/$FILE/$FILE/ $cwd/evaluation/experiment/$FILE/ 8 java #>> progress.log 2>>progress.err &
 
-                # create the exceptions hierarchy from the JDK for a particular project
-                python3 $cwd/evaluation/tools/erec/api/exceptions_hierarchy.py $cwd/evaluation/subjects/$FILE/ $cwd/evaluation/experiment/$FILE/ 8 java #>> progress.log 2>>progress.err &
-	        #break
+		# create the exceptions hierarchy from the JDK for a particular project
+		python3 $cwd/evaluation/tools/erec/api/exceptions_hierarchy.py $cwd/evaluation/subjects/$FILE/ $cwd/evaluation/experiment/$FILE/ 8 java #>> progress.log 2>>progress.err &
+		#break
 
 		# traverse and refine the might thrown exceptions of the nodes in the JDK
-                python3 $cwd/evaluation/tools/erec/api/proj_exceptions_level_2_api.py $cwd/evaluation/subjects/$FILE/ $cwd/evaluation/experiment/$FILE/ 8 java #>> progress.log 2>>progress.err &
+		python3 $cwd/evaluation/tools/erec/api/proj_exceptions_level_2_api.py $cwd/evaluation/subjects/$FILE/ $cwd/evaluation/experiment/$FILE/ 8 java #>> progress.log 2>>progress.err &
 
 
 		python3 $cwd/evaluation/tools/erec/apps/proj_exceptions_app.py $cwd/evaluation/subjects/$FILE $cwd/evaluation/experiment/$FILE/ $cwd/evaluation/experiment/$FILE/ 8 java
 	fi
-	#echo "\n"
-     	#cd ..
-    fi
+#echo "\n"
+	#cd ..
+fi
 done
